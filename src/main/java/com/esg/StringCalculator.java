ce181e05ce7b31dc9568f50965462935e8a72bba
smling <smling9_9@hotmail.com>
2022-10-20 08:50:11 +0100

    add javadoc.

--- src/main/java/com/esg/StringCalculator.java
+++ src/main/java/com/esg/StringCalculator.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 /**
  * String calculator.
+ * Details see READMD.md for more details.
  */
 public class StringCalculator {
     private static final String DEFAULT_DELIMITER = ",";
@@ -17,6 +18,11 @@ public class StringCalculator {
     private static final int DEFAULT_ALLOW_NUMBER_RANGE_START = 0;
     private static final int DEFAULT_ALLOW_NUMBER_RANGE_END = 1000;
 
+    /**
+     * Add numeric value with predefined delimiter(s).
+     * @param numbers numeric value expression.
+     * @return Summary of value.
+     */
     public int add(String numbers) {
         AtomicInteger result = new AtomicInteger();
         if(isNullOrEmpty(numbers)) {
@@ -32,6 +38,12 @@ public class StringCalculator {
         convertedNumbersInRange.forEach(result::addAndGet);
         return result.get();
     }
+
+    /**
+     * Get all custom delimiters and convert to default delimiter.
+     * @param numbers numeric value expressing in custom delimiter(s).
+     * @return numeric value expressing in default delimiter.
+     */
     private String convertWithDelimiter(String numbers) {
         List<String> delimiters = getDelimiter(numbers);
         String convertedNumbers = numbers;
@@ -40,6 +52,12 @@ public class StringCalculator {
         }
         return convertedNumbers;
     }
+
+    /**
+     * Convert integer into list with default delimiter.
+     * @param numbers Numeric value expressing in default delimiter.
+     * @return List of integer.
+     */
     private List<Integer> convertToIntegers(String numbers) {
         return Arrays.stream(numbers.replace(DEFAULT_DELIMITER_SUFFIX,DEFAULT_DELIMITER).split(DEFAULT_DELIMITER)).map(o-> {
             try {
@@ -50,6 +68,11 @@ public class StringCalculator {
         }).collect(Collectors.toList());
     }
 
+    /**
+     * Indicate numeric value express contains custom delimiter or not.
+     * @param numbers numeric value express.
+     * @return Boolean to indicate numeric value express contains custom delimiter or not.
+     */
     private boolean requiredCustomDelimiter(String numbers) {
         if(isNullOrEmpty(numbers) || numbers.length() <= DEFAULT_DELIMITER_PREFIX.length()) {
             return false;
@@ -57,6 +80,11 @@ public class StringCalculator {
         return numbers.startsWith(DEFAULT_DELIMITER_PREFIX);
     }
 
+    /**
+     * Get delimiter(s) into list.
+     * @param numbers numeric value express.
+     * @return List of delimiter(s).
+     */
     private List<String> getDelimiter(String numbers) {
         if(requiredCustomDelimiter(numbers)) {
             int delimiterEndIndex = numbers.indexOf(DEFAULT_DELIMITER_SUFFIX);
@@ -76,14 +104,29 @@ public class StringCalculator {
         return List.of(DEFAULT_DELIMITER);
     }
 
+    /**
+     * Indicate string is null or empty.
+     * @param numbers string value.
+     * @return boolean to indicate string is null or empty or not.
+     */
     private boolean isNullOrEmpty(String numbers) {
         return (Objects.isNull(numbers) || numbers.isEmpty() || numbers.isBlank());
     }
 
+    /**
+     * Get list of negative number(s).
+     * @param numbers List of integer.
+     * @return List of negative number.
+     */
     private List<Integer> getNegativeNumbers(List<Integer> numbers) {
         return numbers.stream().filter(o->o < 0).collect(Collectors.toList());
     }
 
+    /**
+     * Get list of number with in predefined range.
+     * @param source List of integer.
+     * @return List of integer in range.
+     */
     private List<Integer> getInRangeNumbers(List<Integer> source) {
         return source.stream().filter(o-> (o >= StringCalculator.DEFAULT_ALLOW_NUMBER_RANGE_START) && (o <= StringCalculator.DEFAULT_ALLOW_NUMBER_RANGE_END)).collect(Collectors.toList());
     }
